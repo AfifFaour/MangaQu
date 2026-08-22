80dab68716a1ea06a9b2651b49f8342c018f41b3
AfifFaour <afiffaour14@gmail.com>
2026-08-22 17:06:53 +0300

    Fix first-load GitHub Pages SPA routing

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// GitHub Pages serves 404.html for unknown SPA routes. The fallback stores
+// the requested URL in sessionStorage and redirects to /MangaQu/.
+// Restore that original URL before React Router initializes.
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,5 +22,5 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send it to an analytics endpoint. Learn more at https://bit.ly/CRA-vitals
 reportWebVitals();
