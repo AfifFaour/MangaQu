318a4dad8760103402b2a4273011d7b913ba194b
AfifFaour <afiffaour14@gmail.com>
2026-09-04 04:27:03 +0300

    Clean up unnecessary comments in client entry point

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// GitHub Pages serves 404.html for unknown SPA routes. The fallback stores
-// the requested URL in sessionStorage and redirects to /MangaQu/.
-// Restore that original URL before React Router initializes.
 const redirect = sessionStorage.getItem('redirect');
 if (redirect) {
   sessionStorage.removeItem('redirect');
@@ -20,7 +17,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send it to an analytics endpoint. Learn more at https://bit.ly/CRA-vitals
 reportWebVitals();
